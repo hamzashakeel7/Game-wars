565548952990f15ae8ff051a366cace2d3968695
hamzashakeel7 <hamzashakeelhaiderdurrani77@gmail.com>
2025-12-25 22:37:28 +0500

    solving build errors

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBgOFZLR2uuuAkvNC4oR0b0nLHvKr1sOe0",
+  authDomain: "game-wars.firebaseapp.com",
+  databaseURL: "https://game-wars-default-rtdb.firebaseio.com",
+  projectId: "game-wars",
+  storageBucket: "game-wars.firebasestorage.app",
+  messagingSenderId: "783310585542",
+  appId: "1:783310585542:web:a2fd19275569784d1f61c7",
+  measurementId: "G-178CLNJ55W",
+};
